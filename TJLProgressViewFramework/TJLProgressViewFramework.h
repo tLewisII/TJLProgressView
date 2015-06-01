debcbe15e5df63662240b43dc517821a223f7652
Terry Lewis <terry@ploverproductions.com>
2015-06-01 11:54:06 -0500

    Add framework scheme so that Carthage can be used instead of CocoaPods

---
+++ TJLProgressViewFramework/TJLProgressViewFramework.h
@@ -0,0 +1,21 @@
+//
+//  TJLProgressViewFramework.h
+//  TJLProgressViewFramework
+//
+//  Created by Terry Lewis II on 6/1/15.
+//  Copyright (c) 2015 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TJLProgressViewFramework.
+FOUNDATION_EXPORT double TJLProgressViewFrameworkVersionNumber;
+
+//! Project version string for TJLProgressViewFramework.
+FOUNDATION_EXPORT const unsigned char TJLProgressViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TJLProgressViewFramework/PublicHeader.h>
+
+#import <TJLProgressViewFramework/TJLProgressView.h>
+
+
